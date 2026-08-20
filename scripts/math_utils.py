20d056a56173321063288cb3e1c085f5aa24120d
yehiaaf <yyehiaahmedm10@gmail.com>
2026-08-20 21:46:29 +0300

    Refactor math_utils.py to include add and subtract

--- scripts/math_utils.py
+++ scripts/math_utils.py
@@ -1,7 +1,5 @@
-from math_utils import add, subtract
+def add(a, b):
+    return a + b
 
-def test_add():
-    assert add(2, 3) == 5
-
-def test_subtract():
-    assert subtract(5, 3) == 2
+def subtract(a, b):
+    return a - b
